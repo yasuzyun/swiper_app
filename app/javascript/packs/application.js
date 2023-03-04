ee9d4a09ecbe3dc1b26322b575c7cecec1f0ee64
EC2 Default User <yasuzyun@yahoo.co.jp>
2023-03-04 07:18:59 +0000

    Initial Commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "script.js"
 import 'jquery'
+import Swiper from 'swiper/swiper-bundle.js';  //この行を追記
+import 'swiper/swiper-bundle.css';　　　
 
 Rails.start()
 Turbolinks.start()
